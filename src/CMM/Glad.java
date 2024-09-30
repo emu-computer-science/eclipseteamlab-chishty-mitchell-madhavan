fe621e3cfc003e2f1064aed3b6424c44838d2acc
nchishty <nchishty@emich.edu>
2024-09-30 14:27:26 -0400

    Commited for step 19

--- src/CMM/Glad.java
+++ src/CMM/Glad.java
@@ -4,4 +4,7 @@ public class Glad {
 	public static void glad() {
 		System.out.println("We are feeling glad");
 	}
+	public static void chishty() {
+		System.out.println("Nawal created a new method");
+	}
 }
