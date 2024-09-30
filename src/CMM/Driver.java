332817acc0bc948cd9b88b3a70ca84aed4050437
cmitch70 <cmitch70@emich.edu>
2024-09-30 14:16:58 -0400

    Add Sad.java :(

--- src/CMM/Driver.java
+++ src/CMM/Driver.java
@@ -5,5 +5,6 @@ public class Driver {
 		System.out.println("Our team is getting started.");
 		Hungry.hungry();
 		Glad.glad();
+		Sad.sad();
 	}
 }
---
+++ src/CMM/Sad.java
@@ -0,0 +1,7 @@
+package CMM;
+
+public class Sad {
+	public static void sad() {
+		System.out.println("We are all feeling sad");
+	}
+}
